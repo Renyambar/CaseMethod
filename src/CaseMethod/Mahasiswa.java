1d2491c8877508ee093c9d8d70143d0e33315666
Renyambar <ambarwatireny73@gmail.com>
2025-04-17 08:39:04 +0700

    first commit

---
+++ src/CaseMethod/Mahasiswa.java
@@ -0,0 +1,19 @@
+package CaseMethod;
+public class Mahasiswa {
+    String NIM[]={"22001", "22002", "22003"};
+    String nama[]={"Ali Rahman", "Budi Santoso", "Citra Dewi"};
+    String prodi[]={"Informatika", "Informatika", "Informatika"};
+
+    public Mahasiswa(){
+
+    }
+
+    void tampilMahasiswa(){
+        System.out.println("Daftar Mahasiswa: ");
+        for (int i = 0; i < NIM.length; i++) {
+            System.out.println("NIM: "+NIM[i]+"\t|\t"+ "Nama: " + nama[i]+"\t|\t"+"Prodi: "+prodi[i]);
+        }
+    }
+    
+    
+}
